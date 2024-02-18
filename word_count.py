46a4ca0251a68e9605f075b866e195bb3235fba2
kblandonv <kblandonv@unal.edu.co>
2024-02-18 14:27:35 -0500

    #

--- word_count.py
+++ word_count.py
@@ -67,7 +67,8 @@ def save_output(dataframe, output_filename):
         dataframe (pandas.DataFrame): The DataFrame to be saved.
         output_filename (str): The name of the output file.
     """
-    dataframe.to_csv(output_filename, index = False, sep = '\t', header = False)
+    dataframe.to_csv(output_filename, index=False, sep='\t', header=True)
+
 
 
 #
